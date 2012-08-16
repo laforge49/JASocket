2e885390856c8494ce5c51ae58adde1ea8274eed
Bill La Forge <laforge49@gmail.com>
2012-08-16 19:55:22 +0530

    start of raw client

--- src/main/java/org/agilewiki/jasocket/client/RawClient.java
+++ src/main/java/org/agilewiki/jasocket/client/RawClient.java
@@ -1,28 +1,32 @@
 package org.agilewiki.jasocket.client;
 
-import org.agilewiki.jactor.RP;
-import org.agilewiki.jactor.pubsub.publisher.Subscribe;
-import org.agilewiki.jactor.pubsub.subscriber.JASubscriber;
+import org.agilewiki.jactor.lpc.JLPCActor;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.StandardSocketOptions;
 import java.nio.channels.SocketChannel;
 
-public class RawClient extends JASubscriber {
+public class RawClient extends JLPCActor {
     int maxPacketSize;
     SocketChannel socketChannel;
 
-    public void open(String hostName, int port, int maxPacketSize, RP rp)
+    public void open(InetAddress inetAddress, int port, int maxPacketSize)
             throws Exception {
         this.maxPacketSize = maxPacketSize;
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(hostName, port);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, port);
         socketChannel = SocketChannel.open();
         socketChannel.configureBlocking(true);
         socketChannel.setOption(StandardSocketOptions.SO_SNDBUF, maxPacketSize);
         socketChannel.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
         socketChannel.setOption(StandardSocketOptions.TCP_NODELAY, true);
         socketChannel.connect(inetSocketAddress);
-        setActorName(socketChannel.getRemoteAddress().toString());
-        (new Subscribe(this)).send(this, this, rp);
+    }
+
+    public void close() {
+        try {
+            socketChannel.close();
+        } catch (Exception ex) {
+        }
     }
 }
