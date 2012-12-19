fb231147d8a492f3fcde8b84f5583a26bb4f1573
Bill La Forge <laforge49@gmail.com>
2012-12-19 09:07:01 +0530

    new StartupAgent

---
+++ src/main/java/org/agilewiki/jasocket/commands/StartupAgent.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2012 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
+package org.agilewiki.jasocket.commands;
+
+import org.agilewiki.jactor.RP;
+import org.agilewiki.jasocket.application.Application;
+import org.agilewiki.jasocket.application.Startup;
+import org.agilewiki.jasocket.node.Node;
+
+public class StartupAgent extends CommandStringAgent {
+    @Override
+    protected void process(RP rp) throws Exception {
+        String args = getArgString().trim();
+        int i = args.indexOf(' ');
+        final String applicationClassName = args.substring(0, i);
+        if (i > -1)
+            args = args.substring(i + 1).trim();
+        else {
+            args = "";
+        }
+        final String a = args;
+        Node node = agentChannelManager().node;
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Class<Application> applicationClass = (Class<Application>) classLoader.loadClass(applicationClassName);
+        Application application = node.initializeApplication(applicationClass);
+        Startup startup = new Startup(node, args, out);
+        startup.send(this, application, rp);
+    }
+}
